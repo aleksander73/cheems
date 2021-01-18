db041b1662e786180b38a453d96002b678714ec1
aleksander73 <olekdobkowski@gmail.com>
2021-01-18 18:51:25 +0100

    Add Component

---
+++ cheems/src/main/java/aleksander73/cheems/core/Component.java
@@ -0,0 +1,13 @@
+package aleksander73.cheems.core;
+
+public class Component {
+    private GameObject gameObject;
+
+    public GameObject getGameObject() {
+        return gameObject;
+    }
+
+    public void setGameObject(GameObject gameObject) {
+        this.gameObject = gameObject;
+    }
+}
