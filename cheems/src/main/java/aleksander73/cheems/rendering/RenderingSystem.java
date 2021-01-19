5eed6743b8b2db88f0e17dda05731515f02d2f77
aleksander73 <olekdobkowski@gmail.com>
2021-01-19 20:51:34 +0100

    Add Shaders utility

--- cheems/src/main/java/aleksander73/cheems/rendering/RenderingSystem.java
+++ cheems/src/main/java/aleksander73/cheems/rendering/RenderingSystem.java
@@ -21,6 +21,7 @@ public class RenderingSystem extends System {
         GLES20.glDepthFunc(GLES20.GL_LEQUAL);
         GLES20.glEnable(GLES20.GL_BLEND);
         GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+        Shaders.initShaders();
         this.setReady(true);
     }
 
