10b00cdb19744659e0b514c75bdab6a21fdac4f1
aleksander73 <olekdobkowski@gmail.com>
2021-01-19 17:25:11 +0100

    Add Colour

---
+++ cheems/src/main/java/aleksander73/cheems/rendering/materials/Colour.java
@@ -0,0 +1,48 @@
+package aleksander73.cheems.rendering.materials;
+
+import aleksander73.math.linear_algebra.Vector4d;
+
+public class Colour {
+    public static final Colour DEFAULT = new Colour(255, 255, 255, 255);
+    private Vector4d rgba;
+
+    public Colour(int red, int green, int blue, int alpha) {
+        rgba = new Vector4d(red, green, blue, alpha);
+    }
+
+    public Vector4d normalize() {
+        return rgba.mul(1.0f / 255.0f).toVector4d();
+    }
+
+    public int getRed() {
+        return (int) rgba.getW();
+    }
+
+    public void setRed(int red) {
+        rgba.getValues()[0] = red;
+    }
+
+    public int getGreen() {
+        return (int) rgba.getX();
+    }
+
+    public void setGreen(int green) {
+        rgba.getValues()[1] = green;
+    }
+
+    public int getBlue() {
+        return (int) rgba.getY();
+    }
+
+    public void setBlue(int blue) {
+        rgba.getValues()[2] = blue;
+    }
+
+    public int getAlpha() {
+        return (int) rgba.getZ();
+    }
+
+    public void setAlpha(int alpha) {
+        rgba.getValues()[3] = alpha;
+    }
+}
