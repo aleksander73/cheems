89a6569cf528debeb7f10e9ef0b409e9e623857c
aleksander73 <olekdobkowski@gmail.com>
2021-01-21 13:02:35 +0100

    Add PhysicsSystem stub

---
+++ cheems/src/main/java/aleksander73/cheems/physics/PhysicsSystem.java
@@ -0,0 +1,11 @@
+package aleksander73.cheems.physics;
+
+import aleksander73.cheems.core.GameEngine;
+import aleksander73.cheems.core.System;
+
+public class PhysicsSystem extends System {
+    public PhysicsSystem(GameEngine gameEngine) {
+        super(gameEngine);
+        this.setReady(true);
+    }
+}
